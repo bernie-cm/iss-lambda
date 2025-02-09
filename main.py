a4b7194947235141cde6d69db4cc1683b277c1c9
bernie-cm <bernie.camejo@icloud.com>
2025-02-10 08:18:14 +1000

    building main function of app

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
-def main():
-    pass
+import requests
+import pandas as pd
 
-if __name__ == "__main__":
-    main()
+# Get the API response using requests package
+URL = "http://api.open-notify.org/iss-now.json"
+try:
+    response = requests.get(URL)
+    if response.status_code != 200:
+        raise Exception(f"API request failed with status {response.status_code: {response.text}}")
+    # Extract the JSON from the response
+    data = response.json()
+except Exception as e:
+    print(f"Error: {e}")
+else:
+    print(f"Request successful: {data}")
+
+# Convert the response into a dataframe using pandas
+
+# Establish the connection to the database using ???
+
+# Write the information to the Postgres database
+
+# Close the connection
+
+# def main():
+#     pass
+
+# if __name__ == "__main__":
+#     main()
