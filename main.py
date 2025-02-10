403902907398ce63634dc7963fb45ad9ca7fe851
bernie-cm <bernie.camejo@icloud.com>
2025-02-10 10:28:03 +1000

    refactored main function

--- main.py
+++ main.py
@@ -1,29 +1,45 @@
 import requests
 import pandas as pd
+import argparse
+from sqlalchemy import create_engine
 
-# Get the API response using requests package
-URL = "http://api.open-notify.org/iss-now.json"
-try:
-    response = requests.get(URL)
-    if response.status_code != 200:
-        raise Exception(f"API request failed with status {response.status_code: {response.text}}")
-    # Extract the JSON from the response
-    data = response.json()
-except Exception as e:
-    print(f"Error: {e}")
-else:
-    print(f"Request successful: {data}")
+def main(params):
+    username = params.username
+    password = params.password
 
-# Convert the response into a dataframe using pandas
+    # Get the API response using requests package
+    URL = "http://api.open-notify.org/iss-now.json"
+    try:
+        response = requests.get(URL)
+        if response.status_code != 200:
+            raise Exception(f"API request failed with status {response.status_code: {response.text}}")
+        # Extract the JSON from the response
+        data = response.json()
+    except Exception as e:
+        print(f"Error: {e}")
+    else:
+        print(f"Request successful: {data}")
 
-# Establish the connection to the database using ???
+    # Convert the response into a DataFrame using pandas
+    df = pd.DataFrame([{
+        "timestamp": data["timestamp"],
+        "latitude": data["iss_position"]["latitude"],
+        "longitude": data["iss_position"]["longitude"]
+    }])
 
-# Write the information to the Postgres database
+    engine = create_engine(f"postgresql://{username}:{password}@localhost:5432/iss-locations")
 
-# Close the connection
+    # Only needed if table needs to be recreated
+    # df.head(0).to_sql(name="iss-locations", con=engine, if_exists="replace", index=False)
+    df.to_sql(name="iss-locations", con=engine, if_exists="append", index=False)
 
-# def main():
-#     pass
+if __name__ == "__main__":
+    # Create the CLI parser
+    parser = argparse.ArgumentParser(description="Call ISS API and store current position in Amazon RDS")
 
-# if __name__ == "__main__":
-#     main()
+    # Create two CLI arguments to ask for username and password
+    parser.add_argument("-u", help="username for Postgres")
+    parser.add_argument("-p", help="password for Postgres")
+
+    args = parser.parse_args()
+    main(args)
