7df2d03175b8ebb07f4808a306894433adfa606e
bernie-cm <bernie.camejo@icloud.com>
2025-02-17 20:41:39 +1000

    created plotting function

---
+++ plot.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import os
+from sqlalchemy import create_engine
+
+def main():
+    connect_to_database():
+        pass
+
+    convert_sql_table_to_df(sql_table):
+        pass
+
+    create_plot(dataframe):
+        pass
+
+if __name__ == "__main__":
+    main()
