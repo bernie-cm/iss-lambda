b4ccff63d4b83314654aa0cb33a6fe4d6bbfebe9
bernie-cm <bernie.camejo@icloud.com>
2025-02-17 20:49:42 +1000

    added functions to plot.py

--- plot.py
+++ plot.py
@@ -12,5 +12,27 @@ def main():
     create_plot(dataframe):
         pass
 
+def connect_to_database():
+    # Read database credentials from environment variables
+    user = os.getenv("POSTGRES_USER")
+    password = os.getenv("POSTGRES_PASSWORD")
+    host = os.getenv("POSTGRES_HOST")
+
+    # Create the engine connection
+    engine = create_engine(f"postgresql://{user}:{password}@{host}:5432/iss_locations_db")
+
+def convert_sql_table_to_df(sql_table):
+    # This should be changed to read from the database rather than writing to the database
+    # TODO: Research the function to do this
+    df.to_sql(name="iss_locations", con=engine, if_exists="append", index=False)
+
+    # This function should return a dataframe
+    pass
+
+def create_plot(df):
+    pass
+
+
+
 if __name__ == "__main__":
     main()
