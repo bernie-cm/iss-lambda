45a3b65462eb04f97522a38802c0dfad66fc51fe
bernie-cm <bernie.camejo@icloud.com>
2025-02-18 17:44:08 +1000

    finished sql reading function

--- plot.py
+++ plot.py
@@ -2,17 +2,21 @@ import pandas as pd
 import os
 from sqlalchemy import create_engine
 
+DATABASE = "iss_locations_db"
+TABLE = "iss_locations"
+
 def main():
     # Step 1: establish the connection to the AWS RDS db
-    connect_to_database()
+    db_engine = connect_to_database()
 
     # With connection established, read all the data from the table
     # This should give you a dataframe to pass to the next plotting function
-    dataframe = convert_sql_table_to_df(sql_table)
+    dataframe = convert_sql_table_to_df(TABLE, db_engine)
 
     # Having read all the data from Postgres db
     # Call the plotting function
-    create_plot(dataframe)
+    # create_plot(dataframe)
+    print(dataframe)
 
 # ------ FUNCTION DEFINITIONS ------ 
 def connect_to_database():
@@ -22,18 +26,19 @@ def connect_to_database():
     host = os.getenv("POSTGRES_HOST")
 
     # Create the engine connection
-    engine = create_engine(f"postgresql://{user}:{password}@{host}:5432/iss_locations_db")
+    engine = create_engine(f"postgresql://{user}:{password}@{host}:5432/{DATABASE}")
+    return engine
 
-def convert_sql_table_to_df(sql_table):
+def convert_sql_table_to_df(table_name, engine):
     # This should be changed to read from the database rather than writing to the database
-    # TODO: Research the function to do this
-    df.to_sql(name="iss_locations", con=engine, if_exists="append", index=False)
+    df = pd.read_sql_table(table_name, engine)   
 
     # This function should return a dataframe
-    pass
+    return df
+    
 
-def create_plot(df):
-    pass
+#def create_plot(df):
+#    pass
 
 
 
